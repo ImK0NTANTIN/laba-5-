4e1a3e75fe5ea2bb244515336cf4cde638019ab3
KONTANTIN <fantstika_2018@mail.ru>
2023-06-13 01:59:50 +0300

    №1

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
 arr = [5,17,3,9,1]
-for i in range(0, len(arr)):
-    arr[i] = arr[i] * 2
-min = min(arr)
-max = max(arr)
-for i in range(0, len(arr)):
-    if arr[i] == min:
-        arr[i] = arr[i] * 3
-    elif arr[i] == max:
-     arr[i] = arr[i] * 3
-print(arr)
