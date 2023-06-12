95725da944d69f54634a002b2dfe11cdd87ccc40
KONTANTIN <fantstika_2018@mail.ru>
2023-06-13 02:05:09 +0300

    №4

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ for i in range(0, len(arr)):
     arr[i] = arr[i] * 2
 min = min(arr)
 max = max(arr)
+for i in range(0, len(arr)):
+    if arr[i] == min:
+        arr[i] = arr[i] * 3
+    elif arr[i] == max:
+        arr[i] = arr[i] * 3
+
